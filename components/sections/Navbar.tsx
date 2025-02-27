e42e83dc92755fa33f68cb407745be99ea3c40bb
Joaquim <joaquimcolacilli9@gmail.com>
2025-02-27 19:48:34 -0300

    se agrega animacion subrayado en navbar

--- components/sections/Navbar.tsx
+++ components/sections/Navbar.tsx
@@ -68,7 +68,11 @@ export default function Navbar() {
                     } transition-colors duration-300 font-medium`}
                   >
                     {item}
-                    <span className="absolute -bottom-1 left-0 w-0 h-0.5 bg-white group-hover:w-full transition-all duration-300"></span>
+                    <span
+                      className={`absolute -bottom-1 left-0 w-0 h-0.5 transition-all duration-300 ${
+                        isScrolled || isMenuOpen ? "bg-[#A60C38]" : "bg-white"
+                      } group-hover:w-full`}
+                    ></span>
                   </a>
                 ))}
               </div>
